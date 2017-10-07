aa97c4b9ed1fdb972465bf5f08611083d0f9ca25
Darko Meshkovski <darrko_messkovski@hotmail.com>
2017-10-07 17:13:11 +0200

    added group service

--- StudyBuddies.Web/app/common/services/user-service.ts
+++ StudyBuddies.Web/app/common/services/user-service.ts
@@ -2,6 +2,10 @@
     export interface IUserService {
         getUserById(userId: string): angular.IHttpPromise<any>;
         getAllUsers(): angular.IHttpPromise<any>;
+        saveUser(user: any): angular.IHttpPromise<any>;
+        deleteUser(userId: string): angular.IHttpPromise<any>;
+
+        getLatestGroupPosts(userId: string): angular.IHttpPromise<any>;
     }
 
     class UserService implements IUserService {
@@ -14,6 +18,29 @@
 
         getAllUsers(): angular.IHttpPromise<any> {
             return this.$http.get("/api/user");
+        }
+
+        saveUser(user: any): angular.IHttpPromise<any> {
+            const data = {
+                Name: user.name,
+                Surname: user.surname,
+                Email: user.email,
+                Password: user.password
+            };
+
+            return this.$http.post("/api/user", data);
+        }
+
+        deleteUser(userId: string): angular.IHttpPromise<any> {
+            const params = {
+                id: userId
+            };
+
+            return this.$http.delete("/api/user", { params: params });
+        }
+
+        getLatestGroupPosts(userId: string): angular.IHttpPromise<any> {
+            return this.$http.get(`/api/user/${userId}/post`);
         }
     }
 
