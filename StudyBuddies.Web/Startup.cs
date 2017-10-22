f55ecac5dbca98108f4d0de6022e434ec5e8c2e7
Darko Meshkovski <darkom@workflowstoday.com>
2017-10-22 04:21:23 +0200

    re-added sb.web

---
+++ StudyBuddies.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudyBuddies.Web.Startup))]
+namespace StudyBuddies.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
